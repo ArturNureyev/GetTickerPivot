fd9aced43003d69b8c142113effa5ef4bf11587b
ArturNureyev <arturnurey@gmail.com>
2022-03-09 18:13:29 +0300

    error_message

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ def get_ticker_pivot(ticker_symbol):
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
     }
     src = requests.get(url, headers=headers)
-    resistance = []
-    for item in ['r1', 'r2', 'r3']:
-        resistance.append(HTMLParser(src.text).css_first(f'div#{item}').text())
-    support = []
-    for item in ['s1', 's2', 's3']:
-        support.append(HTMLParser(src.text).css_first(f'div#{item}').text())
-    pivots = {'resistance': resistance, 'support': support}
+    if HTMLParser(src.text).css_first('div#pivots') is None:
+        error_message = {'error': 'NO TICKER FOUND'}
+        return error_message
+    else:
+        resistance = []
+        for item in ['r1', 'r2', 'r3']:
+            resistance.append(HTMLParser(src.text).css_first(f'div#{item}').text())
+        support = []
+        for item in ['s1', 's2', 's3']:
+            support.append(HTMLParser(src.text).css_first(f'div#{item}').text())
+        pivots = {'resistance': resistance, 'support': support}
     return pivots
 
